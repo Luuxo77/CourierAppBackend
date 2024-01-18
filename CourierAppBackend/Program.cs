ffd78ea212c802f18bb5bb42ae9d69a4bf0b0073
Łukasz <01171305@pw.edu.pl>
2024-01-18 11:56:03 +0100

    abc

--- CourierAppBackend/Program.cs
+++ CourierAppBackend/Program.cs
@@ -66,6 +66,7 @@ builder.Services.AddScoped<ApiKeyAuthFilter>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
