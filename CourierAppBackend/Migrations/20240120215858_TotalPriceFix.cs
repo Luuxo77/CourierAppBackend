1038fc63f1b37618905fe95b4e5f4b0f4e27e086
Łukasz <01171305@pw.edu.pl>
2024-01-20 23:01:09 +0100

    Added total price to database

---
+++ CourierAppBackend/Migrations/20240120215858_TotalPriceFix.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourierAppBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class TotalPriceFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalPrice",
+                table: "TemporaryOffers",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "TemporaryOffers");
+        }
+    }
+}
--- CourierAppBackend/Models/Database/TemporaryOffer.cs
+++ CourierAppBackend/Models/Database/TemporaryOffer.cs
@@ -7,7 +7,7 @@
         public string InquiryID { get; set; } = string.Empty;
         public int OfferID { get; set; }
         public string Currency { get; set; } = null!;
-        public decimal TotalPrice;
+        public decimal TotalPrice { get; set; }
         public List<PriceItem> PriceItems { get; set; } = null!;
         public DateTime ExpiringAt { get; set; }
     }
