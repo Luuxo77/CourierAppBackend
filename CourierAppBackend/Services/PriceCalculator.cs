38c60999c33d5f243ecee8f9851710489d174c36
Łukasz <01171305@pw.edu.pl>
2024-01-22 12:35:00 +0100

    size fee fix

--- CourierAppBackend/Services/PriceCalculator.cs
+++ CourierAppBackend/Services/PriceCalculator.cs
@@ -6,7 +6,7 @@ namespace CourierAppBackend.Services;
 public class PriceCalculator : IPriceCalculator
 {
     private static readonly decimal WeightMultiplier = 1;
-    private static readonly decimal SizeMultiplier = 0.001M;
+    private static readonly decimal SizeMultiplier = 0.05M;
     public Price CalculatePrice(Inquiry inquiry)
     {
         Price price = new()
@@ -15,7 +15,7 @@ public class PriceCalculator : IPriceCalculator
             PriorityFee = inquiry.HighPriority ? 10 : 0,
             DeliveryAtWeekendFee = inquiry.DeliveryAtWeekend ? 10 : 0,
             WeightFee = Math.Round((decimal)inquiry.Package.Weight * WeightMultiplier, 2, MidpointRounding.ToPositiveInfinity),
-            SizeFee = Math.Round(inquiry.Package.Height * inquiry.Package.Width * inquiry.Package.Length * SizeMultiplier, 2, MidpointRounding.ToPositiveInfinity)
+            SizeFee = Math.Round(inquiry.Package.Height * SizeMultiplier + inquiry.Package.Width * SizeMultiplier + inquiry.Package.Length * SizeMultiplier, 2, MidpointRounding.ToPositiveInfinity)
         };
         price.FullPrice = price.BaseDeliveryPrice + price.PriorityFee + price.DeliveryAtWeekendFee + price.WeightFee + price.SizeFee;
         return price;
